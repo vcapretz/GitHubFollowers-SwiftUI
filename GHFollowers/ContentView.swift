cb46cd9192b5c7a151392d2e83d0a539d1dd821c
Vitor Capretz <capretzvitor@gmail.com>
2022-06-09 16:01:55 -0300

    Initial Commit

---
+++ GHFollowers/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GHFollowers
+//
+//  Created by Vitor Capretz on 09/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GHFollowers/GHFollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowersApp.swift
+//  GHFollowers
+//
+//  Created by Vitor Capretz on 09/06/22.
+//
+
+import SwiftUI
+
+@main
+struct GHFollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
